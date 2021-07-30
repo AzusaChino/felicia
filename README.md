22f893a0ec6874a8dcf830bc940de0306e52f8e3
AzusaChino <azusa146@gmail.com>
2021-07-30 16:49:58 +0800

    add files

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # felicia
+
 Felicia（フェリシア）琉璃雏菊（蓝费莉菊）
+
+## Description
+
+The place I learn python and ML
+
+## Setup
+
+1. run `pip install -r requirements.txt`
---
+++ app.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+import numpy as np
+
+if __name__ == "__main__":
+    a = tf.test()
+    b = np.random()
+    print("Hello %s %s" % a, b)
+    
