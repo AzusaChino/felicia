84f69edd3f4ac9a4becdb734efa62825446a8449
AzusaChino <azusa146@gmail.com>
2021-10-20 15:25:30 +0800

    ft. learn ai with all references

--- README.md
+++ README.md
@@ -4,8 +4,13 @@ Felicia（フェリシア）琉璃雏菊（蓝费莉菊）
 
 ## Description
 
-The place I learn python and ML
+The place where I learn python and AI stuffs
 
 ## Setup
 
-1. run `pip install -r requirements.txt`
+1. create new venv `python -m venv ./venv` (last one is the folder)
+2. install requirements `pip install -r requirements.txt`
+
+## Reference
+
+- [faceai](https://github.com/vipstone/faceai)
--- app.py
+++ app.py
@@ -1,8 +1,21 @@
-import tensorflow as tf
-import numpy as np
+import cv2
+
+
+def main():
+    file_path = "./peek.jpg"
+    img = cv2.imread(file_path)
+    # turn the img to gray
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # draw rectangles
+    x = y = 10  # coordinate axis
+    w = 100  # rect area
+    color = (0, 0, 255)
+    cv2.rectangle(img, (x, y), (x + w, y + w), color, 1)
+    cv2.imshow("Image", img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
 
 if __name__ == "__main__":
-    a = tf.test()
-    b = np.random()
-    print("Hello %s %s" % a, b)
-    
+    main()
