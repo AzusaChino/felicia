17b759e093e5d7906db6944631d4b89deb0c7d83
azusachino <azusachino@proton.me>
2024-01-04 16:18:48 +0900

    s3 config & sample (not done yet)

--- aws/s3.go
+++ aws/s3.go
@@ -1,8 +1,44 @@
 package aws
 
-import "github.com/aws/aws-sdk-go-v2/service/s3"
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+)
+
+const (
+	s3KeyConst    = "S3_ACCESS_KEY"
+	s3SecretConst = "S3_ACCESS_SECRET"
+)
+
+var S3Client *s3.Client
+
+func InitS3() {
+	ctx := context.TODO()
+	cfg, err := config.LoadDefaultConfig(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	s3Key := os.Getenv(s3KeyConst)
+	s3Secret := os.Getenv(s3SecretConst)
+	creds := credentials.NewStaticCredentialsProvider(s3Key, s3Secret, "")
+	cfg.Credentials = creds
+	S3Client = s3.NewFromConfig(cfg)
+}
 
 func ListS3Files() {
-	opt := &s3.Options{}
-	s3.New(*opt)
+	ctx := context.Background()
+	result, err := S3Client.ListBuckets(ctx, nil)
+	if err != nil {
+		log.Print("fail to list buckets")
+	}
+
+	for _, bucket := range result.Buckets {
+		fmt.Println(*bucket.Name)
+	}
 }
--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"log"
+
+	"github.com/azusachino/felicia/aws"
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatal("fail to read environment variables")
+	}
+
+	aws.InitS3()
+}
+
 func main() {
-	println("Hello, world!")
+	aws.ListS3Files()
 }
